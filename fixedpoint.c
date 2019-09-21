40518a29407adc30a352355dc171927f5824da6c
Katherine Gerot <katie.gerot@huskers.unl.edu>
2019-09-21 00:33:55 -0500

    unreviewed secant, fixed

--- fixedpoint.c
+++ fixedpoint.c
@@ -3,7 +3,7 @@
 * Date: 09/20/2019
 * Class: CSCE 440
 * 
-* Program computing the Bisection Method with the following equations:
+* Program computing Newton's Method with the following equations:
 * (a) 3x^3 - 2x^2 + 5x - 2e^x + 1
 * (b) x^4 + 3x^2 - 2
 * (c) 3x - x^2 - e^x - 2
@@ -20,39 +20,23 @@ int main(int argc, char **argv) {
 	const double TOL = 0.0001; //tolerance
 	const int MAX_I = 50; //maximum iterations
 	char eq = argv[1][0]; //equation, either a, b, c, or d
-	double a, b, fa, fp, p; // endpoints
+	double p; 
+	double p0 = 1;
 	
-	switch(eq) { //sets correct interval
-		case 'a': 
-			a = -1;
-			b = 1;
-			break;
-		case 'b':
-			a = 0;
-			b = 2;
-			break;
-		case 'c':
-			a = -2;
-			b = 2;
-			break;
-		case 'd':
-			a = -2;
-			b = 2;
-			break;
-		default: exit(1);
-	}
 	printf("i\tp\n"); //headers for output table
 	int i = 0;
-	fa = f(a, eq); //f(a)
 	
-	//FIXED POINT METHOD
+	//NEWTON'S METHOD
 	while(i <= MAX_I) {
 		i++;
-		
-		//...
-		
+		printf("%2d", i);
+		p = f(p0, eq);
+		if(fabs(p - p0) < TOL) {
+			printf("%14.6lf\n", p);
+			break;
+		}			
 		printf("%14.6lf\n", p);
-		
+		p0 = p;	
 	}
 	return 0;
 }
@@ -78,5 +62,4 @@ double f(double x, char e) {
 			return x- cos(x);
 		default: exit(1);
 	}
-	
 }
